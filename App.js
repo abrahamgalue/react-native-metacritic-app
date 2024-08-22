b2ae4e1d4bf01beb573e7eca56d0a9dcd042aa23
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-22 13:07:51 -0400

    Expo and prettier linter installed and set up

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+
+const icon = require('./assets/icon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Pressable
+        onPress={() => {
+        }}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+          },
+          styles.wrapperCustom,
+        ]}>
+        {({ pressed }) => (
+          <Text style={{ ...styles.text, fontSize: pressed ? 36 : 24 }}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+        )}
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +27,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textColor: {
+    color: "#fff"
+  }
 });
