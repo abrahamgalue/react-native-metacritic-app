5d89eff94bbb140949d1143656a06d1fdb5614b5
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-24 16:34:23 -0400

    Added svg brand logo

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,26 +1,29 @@
-/* eslint-disable prettier/prettier */
 import { useEffect, useState } from 'react'
 import { getLatestGames } from '../lib/metacritic'
 import { View, ActivityIndicator, FlatList } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { GameCard } from './GameCard'
+import { Logo } from './Logo'
 
 export function Main() {
   const [games, setGames] = useState([])
   const insets = useSafeAreaInsets()
 
   useEffect(() => {
-    getLatestGames().then(games => setGames(games))
+    getLatestGames().then((games) => setGames(games))
   }, [])
 
   return (
     <View style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+      <View style={{ marginBottom: 20 }}>
+        <Logo />
+      </View>
       {games.length === 0 ? (
         <ActivityIndicator size={'large'} />
       ) : (
         <FlatList
           data={games}
-          keyExtractor={game => game.slug}
+          keyExtractor={(game) => game.slug}
           renderItem={({ item }) => <GameCard game={item} />}
         />
       )}
