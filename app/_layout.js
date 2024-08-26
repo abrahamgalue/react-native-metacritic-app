c0a41bb69735dcfa4bcc91c17dd31677a8ac923e
Abraham Galue <abrahamgalue512@gmail.com>
2024-08-26 12:00:17 -0400

    Added stack for navigating through the application natively

--- app/_layout.js
+++ app/_layout.js
@@ -1,10 +1,31 @@
-import { Slot } from 'expo-router'
-import { View } from 'react-native'
+import { Screen } from '../components/Screen'
+import { Link, Stack } from 'expo-router'
+import { Pressable } from 'react-native'
+import { Logo } from '../components/Logo'
+import { CircleInfoIcon } from '../components/Icons'
 
 export default function Layout() {
   return (
-    <View className={'flex-1 bg-black items-center justify-center'}>
-      <Slot />
-    </View>
+    <Screen>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          contentStyle: {
+            backgroundColor: '#000',
+          },
+          headerTitle: '',
+          headerLeft: () => <Logo />,
+          headerRight: () => (
+            <Link asChild href="/about" className="text-blue-400 text-xl">
+              <Pressable>
+                <CircleInfoIcon />
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+    </Screen>
   )
 }
